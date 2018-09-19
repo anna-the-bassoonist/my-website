dcd2abc5afe8944d9b60e9ef737037e1a1d27450
annafagocistka <anafagocistka@gmail.com>
2018-09-19 15:54:44 +0200

    nav-works 100%

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
-//$(document).ready(function () {
-//    $(window).scroll(function () {
-//        if ($(this).scrollTop() > 50) {
-//            $('#main-nav').addClass('changeColor');
-//        }
-//        if ($(this).scrollTop() < 50) {
-//            $('#main-nav').removeClass('changeColor');
-//        }
-//    });
-//
-//});
-//
-//$(document).ready(function () {
-//    $('a[href^="#"]').on('click', function (event) {
-//        var target = $($(this).attr('href'));
-//        if (target.length) {
-//            event.preventDefault();
-//            $('html, body').animate({
-//                scrollTop: target.offset().top - 50
-//            }, 800);
-//        }
-//    });
-//
-//    $('a[href^="#"]').click(function () {
-//        $('#main-nav').slideUp("fast", function () {
-//            $(this).slideDown("slow");
-//        });
-//    });
-//
-//    $(".nav-item").click(function () {
-//        $(".nav-item").removeClass('activeMenu');
-//        $(this).addClass('activeMenu');
-//    });
-//
-//});
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#main-nav').addClass('changeColor');
+        }
+        if ($(this).scrollTop() < 50) {
+            $('#main-nav').removeClass('changeColor');
+        }
+    });
+
+});
+
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - 40
+            }, 800);
+        }
+    });
+
+    $('a[href^="#"]').click(function () {
+        $('#main-nav').slideUp("fast", function () {
+            $(this).slideDown("slow");
+        });
+    });
+
+    $(".nav-item").click(function () {
+        $(".nav-item").removeClass('activeMenu');
+        $(this).addClass('activeMenu');
+    });
+
+});
