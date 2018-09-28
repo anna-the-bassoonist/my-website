8436a1e4f018d82fc2a04380a693a5a8e94fe065
annafagocistka <anafagocistka@gmail.com>
2018-09-28 21:33:50 +0200

    dropdown-menu end

--- js/main.js
+++ js/main.js
@@ -11,40 +11,28 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('a[href^="#"]').on('click', function (event) {
-        var target = $($(this).attr('href'));
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top - 40
-            }, 800);
-        }
-    });
+            $('a[href^="#"]').on('click', function (event) {
+                var target = $($(this).attr('href'));
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 800);
+                }
+            });
 
-    $('a[href^="#"]').click(function () {
-        $('#main-nav').slideUp("fast", function () {
-            $(this).slideDown("slow");
-        });
-    });
+            $('a[href^="#"]').click(function () {
+                $('#main-nav').slideUp("fast", function () {
+                    $(this).slideDown("slow");
+                });
+            });
 
-    $(".nav-item").click(function () {
-        $(".nav-item").removeClass('activeMenu');
-        $(this).addClass('activeMenu');
-    });
+            $(".nav-item").click(function () {
+                $(".nav-item").removeClass('activeMenu');
+                $(this).addClass('activeMenu');
+            });
 
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    let myLanguage = document.getElementById('language').value;
-    console.log('dziala');
-})
-
-//Selection of language
-
-//$(document).ready(function () {
-//    function updateText() {
-//        language = $('#language option:selected').val();
-//        console.log('dziala');
-//    }
-//
-//})
+            $(".language-select").click(function () {
+                
+                }
+            });
