299eb4f6a54816d69f0802fb62b42b697380d2f1
annafagocistka <anafagocistka@gmail.com>
2019-02-07 23:23:53 +0100

    ipad changes back sprachen menu doesnt work properly

--- js/main.js
+++ js/main.js
@@ -52,11 +52,6 @@ $(document).ready(function () {
             $(this).slideDown("slow");
         });
     });
-     if (/iP(hone|od|ad)/.test(navigator.platform)) {
-        $("*").css({
-            "cursor": "pointer"
-        });
-    }
     $('.btn-header').click(function (event) {
         var target = $('#contact');
         event.preventDefault();
@@ -81,6 +76,12 @@ $(document).ready(function () {
             navMobile.collapse('hide');
         });
     });
+
+    if (/iP(hone|od|ad)/.test(navigator.platform)) {
+        $("*").css({
+            "cursor": "pointer"
+        });
+    }
 
     $(".language-select").click(function () {
         i18next.changeLanguage($(this).attr('id'), function () {
